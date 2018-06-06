555f278b1144b26f3b38b1a181d5dafdcf68d4dc
Graham Ballantyne <grahamb@sfu.ca>
2018-06-06 14:53:16 -0700

    Use env var for graphql endpoint

--- src/index.js
+++ src/index.js
@@ -6,8 +6,10 @@ import './index.css';
 import App from './features/App';
 import registerServiceWorker from './registerServiceWorker';
 
+const { REACT_APP_GRAPHQL_ENDPOINT } = process.env;
+console.log(REACT_APP_GRAPHQL_ENDPOINT);
 const client = new ApolloClient({
-  uri: 'http://pj.docker/graphql',
+  uri: REACT_APP_GRAPHQL_ENDPOINT,
 });
 
 class Root extends Component {
