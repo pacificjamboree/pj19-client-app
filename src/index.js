fa36de34ff9202c40d92267c819f502b4898c8c9
Graham Ballantyne <grahamb@sfu.ca>
2018-06-07 10:43:23 -0700

    Eject apollo-boost

--- src/index.js
+++ src/index.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
 import { ApolloProvider } from 'react-apollo';
-import ApolloClient from 'apollo-boost';
+
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { setContext } from 'apollo-link-context';
+
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './features/App';
 import registerServiceWorker from './registerServiceWorker';
 
 const { REACT_APP_GRAPHQL_ENDPOINT } = process.env;
-console.log(REACT_APP_GRAPHQL_ENDPOINT);
+
+const httpLink = new HttpLink({ uri: REACT_APP_GRAPHQL_ENDPOINT });
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
 const client = new ApolloClient({
-  uri: REACT_APP_GRAPHQL_ENDPOINT,
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
 });
 
 class Root extends Component {
