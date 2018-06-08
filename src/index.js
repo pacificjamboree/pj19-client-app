227a7d9b6be8a5883592fccc1459c7354fd5ceac
Graham Ballantyne <grahamb@sfu.ca>
2018-06-08 13:30:51 -0700

    Revert back to apollo-boost

--- src/index.js
+++ src/index.js
@@ -1,41 +1,33 @@
 import React, { Component } from 'react';
 import { ApolloProvider } from 'react-apollo';
-import { ApolloClient } from 'apollo-client';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { HttpLink } from 'apollo-link-http';
-import { withClientState } from 'apollo-link-state';
-import { setContext } from 'apollo-link-context';
-import { ApolloLink } from 'apollo-link';
+import ApolloClient from 'apollo-boost';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './features/App';
 import registerServiceWorker from './registerServiceWorker';
 import { defaults, resolvers } from './graphql';
 
-const { REACT_APP_GRAPHQL_ENDPOINT } = process.env;
-
-const httpLink = new HttpLink({ uri: REACT_APP_GRAPHQL_ENDPOINT });
-
-const authLink = setContext((_, { headers }) => {
-  const { REACT_APP_JWT_NAME } = process.env;
-  const token = localStorage.getItem(REACT_APP_JWT_NAME);
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : '',
-    },
-  };
-});
-const cache = new InMemoryCache();
-const stateLink = withClientState({
-  cache,
-  resolvers,
-  defaults,
-});
+const { REACT_APP_GRAPHQL_ENDPOINT, REACT_APP_JWT_NAME } = process.env;
 
 const client = new ApolloClient({
-  link: ApolloLink.from([authLink, stateLink, httpLink]),
-  cache,
+  uri: REACT_APP_GRAPHQL_ENDPOINT,
+  request: operation => {
+    operation.setContext(({ headers }) => {
+      const token = localStorage.getItem(REACT_APP_JWT_NAME);
+      if (token) {
+        return {
+          headers: {
+            ...headers,
+            authorization: `Bearer ${token}`,
+          },
+        };
+      }
+    });
+  },
+  clientState: {
+    resolvers,
+    defaults,
+  },
 });
 
 class Root extends Component {
