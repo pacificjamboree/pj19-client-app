bdec1a8f294b6cf57442680095019abcb23ad73a
Graham Ballantyne <grahamb@sfu.ca>
2018-06-13 23:52:33 -0700

    Pretty-up the loigin form

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -1,50 +1,81 @@
 import React, { Component } from 'react';
-
+import {
+  Grid,
+  Form,
+  Segment,
+  Container,
+  Button,
+  Header,
+  Message,
+} from 'semantic-ui-react';
 class LoginForm extends Component {
-  constructor() {
-    super();
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+  state = {
+    username: '',
+    password: '',
+  };
+
+  handleChange = (e, { name, value }) => this.setState({ [name]: value });
 
   handleSubmit = async e => {
+    const { username, password } = this.state;
     e.preventDefault();
     await this.props.mutate({
       variables: {
-        username: this.username.value,
-        password: this.password.value,
+        username,
+        password,
       },
     });
-    this.username.value = '';
-    this.password.value = '';
   };
 
   render() {
+    const { error } = this.props;
     return (
-      <div>
-        <h2>Login</h2>
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Username
-            <input
-              autoFocus={true}
-              ref={node => {
-                this.username = node;
-              }}
-            />
-          </label>
+      <Container style={{ paddingTop: '7em' }}>
+        <Grid
+          textAlign="center"
+          style={{ height: '100%' }}
+          verticalAlign="middle"
+        >
+          <Grid.Column style={{ maxWidth: 450 }}>
+            <Header as="h2" textAlign="left">
+              Log in to start your PJ Adventure
+            </Header>
+            {(error &&
+              error.graphQLErrors.map(x => (
+                <Message negative>{x.message}</Message>
+              ))) ||
+              null}
 
-          <label>
-            Password
-            <input
-              type="password"
-              ref={node => {
-                this.password = node;
-              }}
-            />
-          </label>
-          <button type="submit">Login</button>
-        </form>
-      </div>
+            <Form size="large" onSubmit={this.handleSubmit}>
+              <Segment stacked>
+                <Form.Input
+                  name="username"
+                  fluid
+                  icon="user"
+                  iconPosition="left"
+                  placeholder="Username"
+                  onChange={this.handleChange}
+                />
+                <Form.Input
+                  name="password"
+                  fluid
+                  icon="lock"
+                  iconPosition="left"
+                  placeholder="Password"
+                  type="password"
+                  onChange={this.handleChange}
+                />
+                <Button color="teal" fluid size="large">
+                  Login
+                </Button>
+              </Segment>
+            </Form>
+            <Message>
+              <a href="#">Forgot your Password?</a>
+            </Message>
+          </Grid.Column>
+        </Grid>
+      </Container>
     );
   }
 }
