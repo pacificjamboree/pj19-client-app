4ba9fe3c3be8ec5663e02660ef06d4400e375871
Graham Ballantyne <grahamb@sfu.ca>
2018-06-14 14:53:58 -0700

    Change title depending on breakpoint

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,20 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from '@reach/router';
-import { Container, Menu } from 'semantic-ui-react';
+import { Container, Menu, Icon, Responsive } from 'semantic-ui-react';
 import LoginLogout from '../LoginLogout';
 
-export default () => (
-  <header>
-    <Menu color="teal" inverted borderless fixed="top">
-      <Container>
-        <Menu.Item header className="borderless">
-          <Link to="/">Pacific Jamboree 2019 – Adventure</Link>
-        </Menu.Item>
+class NavBar extends Component {
+  state = {};
 
-        <Menu.Menu position="right">
-          <LoginLogout />
-        </Menu.Menu>
-      </Container>
-    </Menu>
-  </header>
-);
+  handleOnUpdate = (e, { width }) => this.setState({ width });
+
+  render() {
+    const { width } = this.state;
+    const headerText =
+      width >= Responsive.onlyMobile.maxWidth
+        ? 'Pacific Jamboree 2019 – Adventure'
+        : 'PJ 2019 – Adventure';
+    return (
+      <Responsive as="header" fireOnMount onUpdate={this.handleOnUpdate}>
+        <Menu color="teal" inverted borderless fixed="top">
+          <Container style={{ width: '100%' }}>
+            <Menu.Item>
+              <Icon name="sidebar" />
+            </Menu.Item>
+            <Menu.Item header className="borderless">
+              <Link to="/">{headerText}</Link>
+            </Menu.Item>
+
+            <Menu.Menu position="right">
+              <LoginLogout />
+            </Menu.Menu>
+          </Container>
+        </Menu>
+      </Responsive>
+    );
+  }
+}
+
+export default NavBar;
