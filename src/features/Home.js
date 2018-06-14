7c98f548f76952752d31015648282d444744b826
Graham Ballantyne <grahamb@sfu.ca>
2018-06-14 15:20:55 -0700

    Home feature placeholder

--- src/features/Home.js
+++ src/features/Home.js
@@ -1,9 +1,23 @@
 import React from 'react';
-
+import { Image, Header } from 'semantic-ui-react';
 const Home = () => (
   <div>
-    <h2>Home</h2>
-    <p>Welcome to PJ 2019 Adventure</p>
+    <Image src="https://ucafd4111992e9725f2f058bb919.dl.dropboxusercontent.com/cd/0/get/AI7JSmQgS1s0W8eAyCsJUbhp6JsOvL5lQzy3n16YMchXwZ-aoV1oA19mfcliJ27n2qLKTPi10-lRcZcAWrRRKf0yrP3jFNRvNpUvdCjiF5T5TSa3ZS8oeEgSvAbUHCD0wfVymczWMOkGryfYZ6xUrcdEKaiY28re5mD3cheCoJL5iLFnY0wm7St2wmS5npAIzSTnBM69YulVKXLHPskXGkyR/file" />
+    <Header as="h1" textAlign="center">
+      Get ready for your PJ Adevnture!
+    </Header>
+    <img
+      style={{ width: '100%', marginTop: '2em' }}
+      src="https://react.semantic-ui.com//assets/images/wireframe/paragraph.png"
+    />
+    <img
+      style={{ width: '100%', marginTop: '2em' }}
+      src="https://react.semantic-ui.com//assets/images/wireframe/paragraph.png"
+    />
+    <img
+      style={{ width: '100%', marginTop: '2em' }}
+      src="https://react.semantic-ui.com//assets/images/wireframe/paragraph.png"
+    />
   </div>
 );
 
