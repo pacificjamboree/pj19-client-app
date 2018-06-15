eb086f1f8d3de8b9a7691f2324ae267cbd742fb6
Graham Ballantyne <grahamb@sfu.ca>
2018-06-15 15:13:33 -0700

    TEMP Home changes

--- src/features/Home.js
+++ src/features/Home.js
@@ -2,21 +2,24 @@ import React from 'react';
 import { Image, Header } from 'semantic-ui-react';
 const Home = () => (
   <div>
-    <Image src="https://ucafd4111992e9725f2f058bb919.dl.dropboxusercontent.com/cd/0/get/AI7JSmQgS1s0W8eAyCsJUbhp6JsOvL5lQzy3n16YMchXwZ-aoV1oA19mfcliJ27n2qLKTPi10-lRcZcAWrRRKf0yrP3jFNRvNpUvdCjiF5T5TSa3ZS8oeEgSvAbUHCD0wfVymczWMOkGryfYZ6xUrcdEKaiY28re5mD3cheCoJL5iLFnY0wm7St2wmS5npAIzSTnBM69YulVKXLHPskXGkyR/file" />
+    <Image src="https://i.imgur.com/TaoXQmv.jpg" alt="Horse" />
     <Header as="h1" textAlign="center">
       Get ready for your PJ Adevnture!
     </Header>
     <img
       style={{ width: '100%', marginTop: '2em' }}
       src="https://react.semantic-ui.com//assets/images/wireframe/paragraph.png"
+      alt="text"
     />
     <img
       style={{ width: '100%', marginTop: '2em' }}
       src="https://react.semantic-ui.com//assets/images/wireframe/paragraph.png"
+      alt="text"
     />
     <img
       style={{ width: '100%', marginTop: '2em' }}
       src="https://react.semantic-ui.com//assets/images/wireframe/paragraph.png"
+      alt="text"
     />
   </div>
 );
