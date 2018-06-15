f779de186e45012826fc135cd38f06e6cdedd84f
Graham Ballantyne <grahamb@sfu.ca>
2018-06-15 16:23:40 -0700

    Make the sidebar toggle work
    Need to fix the fixed footer…

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 import { Link } from '@reach/router';
 import { Container, Menu, Icon, Responsive } from 'semantic-ui-react';
+import { Query, Mutation } from 'react-apollo';
 import LoginLogout from '../LoginLogout';
+import {
+  GET_NAVBAR_VISIBILITY_STATE,
+  UPDATE_NAVBAR_VISIBILITY_STATE,
+} from '../../graphql/queries';
 
 class NavBar extends Component {
   state = {};
@@ -15,22 +20,45 @@ class NavBar extends Component {
         ? 'Pacific Jamboree 2019 – Adventure'
         : 'PJ 2019 – Adventure';
     return (
-      <Responsive as="header" fireOnMount onUpdate={this.handleOnUpdate}>
-        <Menu color="teal" inverted borderless fixed="top">
-          <Container style={{ width: '100%' }}>
-            <Menu.Item>
-              <Icon name="sidebar" />
-            </Menu.Item>
-            <Menu.Item header className="borderless">
-              <Link to="/">{headerText}</Link>
-            </Menu.Item>
+      <Query query={GET_NAVBAR_VISIBILITY_STATE}>
+        {({ data: { navbarVisible } }) => (
+          <Mutation
+            mutation={UPDATE_NAVBAR_VISIBILITY_STATE}
+            variables={{
+              visible: !navbarVisible,
+            }}
+          >
+            {(toggle, { data, error }) => (
+              <Responsive
+                as="header"
+                fireOnMount
+                onUpdate={this.handleOnUpdate}
+              >
+                <Menu color="teal" inverted borderless fixed="top">
+                  <Container style={{ width: '100%' }}>
+                    <Menu.Item
+                      onClick={toggle}
+                      role="button"
+                      active={navbarVisible}
+                      as="button"
+                      style={{ border: 'none' }}
+                    >
+                      <Icon name="sidebar" />
+                    </Menu.Item>
+                    <Menu.Item header className="borderless">
+                      <Link to="/">{headerText}</Link>
+                    </Menu.Item>
 
-            <Menu.Menu position="right">
-              <LoginLogout />
-            </Menu.Menu>
-          </Container>
-        </Menu>
-      </Responsive>
+                    <Menu.Menu position="right">
+                      <LoginLogout />
+                    </Menu.Menu>
+                  </Container>
+                </Menu>
+              </Responsive>
+            )}
+          </Mutation>
+        )}
+      </Query>
     );
   }
 }
--- src/features/App.js
+++ src/features/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Query } from 'react-apollo';
 import { Router } from '@reach/router';
 import { Container } from 'semantic-ui-react';
@@ -8,27 +8,54 @@ import Logout from './Logout';
 import Adventures from './Adventures';
 import NavBar from '../components/NavBar';
 import Footer from '../components/Footer';
+import SideNav from '../components/SideNav';
+
 import { GET_VIEWER_USERNAME } from '../graphql/queries';
 
-const App = () => (
-  <Query fetchPolicy="network-only" query={GET_VIEWER_USERNAME}>
-    {() => (
-      <div
-        style={{ display: 'flex', minHeight: '100vh', flexDirection: 'column' }}
-      >
-        <NavBar />
-        <Container style={{ flex: 1, paddingTop: '6em' }}>
-          <Router>
-            <Home path="/" />
-            <Login path="/login" />
-            <Logout path="/logout" />
-            <Adventures path="/adventures" />
-          </Router>
-        </Container>
-        <Footer />
-      </div>
-    )}
-  </Query>
-);
+class App extends Component {
+  render() {
+    return (
+      <Query fetchPolicy="network-only" query={GET_VIEWER_USERNAME}>
+        {() => (
+          <div
+            style={{
+              display: 'flex',
+              minHeight: '100vh',
+              flexDirection: 'column',
+            }}
+          >
+            <SideNav>
+              <NavBar />
+
+              <Container
+                style={{ flex: 1, paddingTop: '6em', paddingBottom: '2em' }}
+              >
+                <Router>
+                  <Home path="/" />
+                  <Login path="/login" />
+                  <Logout path="/logout" />
+                  <Adventures path="/adventures" />
+                </Router>
+              </Container>
+            </SideNav>
+            <Footer />
+          </div>
+        )}
+      </Query>
+    );
+  }
+}
 
 export default App;
+
+/*
+logged out
+- / --> Home
+- /login --> Login
+- /adventures
+- /adventure/:id
+- /adventure_guide
+- /faq
+
+
+*/
