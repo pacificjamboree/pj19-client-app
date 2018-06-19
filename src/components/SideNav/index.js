619b1b7683d1a9c122419b30217fe1544f6d55c1
Graham Ballantyne <grahamb@sfu.ca>
2018-06-19 09:11:51 -0700

    Refactor SideNav to use compose() and query()

--- src/components/SideNav/index.js
+++ src/components/SideNav/index.js
@@ -1,54 +1,86 @@
 import React from 'react';
-import { Query } from 'react-apollo';
+import { compose, graphql, Mutation } from 'react-apollo';
 import { Sidebar, Menu } from 'semantic-ui-react';
 import { Link } from '@reach/router';
+import { UPDATE_NAVBAR_VISIBILITY_STATE } from '../../graphql/queries';
+import {
+  GET_NAVBAR_VISIBILITY_STATE,
+  GET_LOGIN_STATE,
+} from '../../graphql/queries';
 
-import { GET_NAVBAR_VISIBILITY_STATE } from '../../graphql/queries';
-
-const SideNav = ({ children }) => (
-  <Query query={GET_NAVBAR_VISIBILITY_STATE}>
-    {({ data: { navbarVisible } }) => {
-      return (
-        <Sidebar.Pushable>
-          <Sidebar
-            as={Menu}
-            animation="push"
-            width="thin"
-            visible={navbarVisible}
-            vertical
-            inverted
-          >
-            <Menu.Item>
-              <Link to="/">Home</Link>
-            </Menu.Item>
-            <Menu.Item>
-              <Link to="/adventures">Adventures</Link>
-            </Menu.Item>
-            <Menu.Item>
-              <Link to="/adventure_guide">Adventure Guide</Link>
-            </Menu.Item>
-            <Menu.Item>
-              <Link to="/faq">FAQ</Link>
-            </Menu.Item>
-            <Menu.Item>
-              <Link to="/Contact">Contact</Link>
-            </Menu.Item>
-          </Sidebar>
-          <Sidebar.Pusher
-            style={{
-              minHeight: '100vh',
-              display: 'grid',
-              gridTemplateRows: 'auto 1fr auto',
-              gridTemplateColumns: '100%',
-            }}
-          >
+const ToggleLink = ({ to, children }) => {
+  return (
+    <Mutation
+      mutation={UPDATE_NAVBAR_VISIBILITY_STATE}
+      variables={{
+        visible: false,
+      }}
+    >
+      {toggle => {
+        return (
+          <Link to={to} onClick={toggle}>
             {children}
-          </Sidebar.Pusher>
-        </Sidebar.Pushable>
-      );
-    }}
-  </Query>
-);
+          </Link>
+        );
+      }}
+    </Mutation>
+  );
+};
 
-export default SideNav;
-//
+const SideNav = props => {
+  const { children } = props;
+  const { loggedIn } = props.getLoginStateQuery;
+  const { navbarVisible } = props.getNavbarVisibilityStateQuery;
+  return (
+    <Sidebar.Pushable>
+      <Sidebar
+        as={Menu}
+        animation="push"
+        width="thin"
+        visible={navbarVisible}
+        vertical
+        inverted
+      >
+        <Menu.Item>
+          {!loggedIn ? (
+            <ToggleLink to="/login">Login</ToggleLink>
+          ) : (
+            <ToggleLink to="/logout">Logout</ToggleLink>
+          )}
+        </Menu.Item>
+        <Menu.Item>
+          <ToggleLink to="/">Home</ToggleLink>
+        </Menu.Item>
+        <Menu.Item>
+          <ToggleLink to="/adventures">Adventures</ToggleLink>
+        </Menu.Item>
+        <Menu.Item>
+          <ToggleLink to="/adventure_guide">Adventure Guide</ToggleLink>
+        </Menu.Item>
+        <Menu.Item>
+          <ToggleLink to="/faq">FAQ</ToggleLink>
+        </Menu.Item>
+        <Menu.Item>
+          <ToggleLink to="/Contact">Contact</ToggleLink>
+        </Menu.Item>
+      </Sidebar>
+      <Sidebar.Pusher
+        style={{
+          minHeight: '100vh',
+          display: 'grid',
+          gridTemplateRows: 'auto 1fr auto',
+          gridTemplateColumns: '100%',
+        }}
+      >
+        {children}
+      </Sidebar.Pusher>
+    </Sidebar.Pushable>
+  );
+};
+
+export default compose(
+  graphql(GET_NAVBAR_VISIBILITY_STATE, {
+    name: 'getNavbarVisibilityStateQuery',
+  }),
+  graphql(GET_LOGIN_STATE, { name: 'getLoginStateQuery' })
+)(SideNav);
