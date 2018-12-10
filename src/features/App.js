d90bd587a29052645eab5bffd1eb99e965e39736
Graham Ballantyne <grahamb@sfu.ca>
2018-12-09 17:59:57 -0800

    Add ErrorBoundary

--- src/features/App.js
+++ src/features/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Query } from 'react-apollo';
 import { Router } from '@reach/router';
 import { Container } from 'semantic-ui-react';
-import Flash from '../components/Flash';
+import FlashMessages from '../components/FlashMessages';
 import Home from './Home';
 import Login from './Login';
 import Logout from './Logout';
@@ -18,66 +18,69 @@ import Footer from '../components/Footer';
 import SideNav from '../components/SideNav';
 import NotFound from './NotFound';
 import AuthorizedRoute from '../components/AuthorizedRoute';
+import ErrorBoundary from '../components/ErrorBoundary';
 
 import { GET_VIEWER_USERNAME } from '../graphql/queries';
 
 class App extends Component {
   render() {
     return (
-      <Query fetchPolicy="network-only" query={GET_VIEWER_USERNAME}>
-        {() => (
-          <div
-            style={{
-              minHeight: '100%',
-              display: 'grid',
-              gridTemplateRows: 'auto 1fr auto',
-              gridTemplateColumns: '100%',
-            }}
-          >
-            <SideNav>
-              <NavBar />
+      <ErrorBoundary>
+        <Query fetchPolicy="network-only" query={GET_VIEWER_USERNAME}>
+          {() => (
+            <div
+              style={{
+                minHeight: '100%',
+                display: 'grid',
+                gridTemplateRows: 'auto 1fr auto',
+                gridTemplateColumns: '100%',
+              }}
+            >
+              <SideNav>
+                <NavBar />
 
-              <Container style={{ paddingTop: '6em', paddingBottom: '2em' }}>
-                <Flash />
-                <Router>
-                  <Home path="/" />
-                  <Login path="/login" />
-                  <Logout path="/logout" />
-                  <Adventures path="/adventures" />
-                  <AdventureDetail path="/adventures/:id" />
-                  <AuthorizedRoute
-                    userRoles={['adventureManager', 'admin']}
-                    path="/adventures/:id/edit"
-                    component={AdventureEdit}
-                  />
-                  <AuthorizedRoute
-                    userRoles={['adventureManager', 'admin']}
-                    path="/oos"
-                    component={OOSList}
-                  />
-                  <AuthorizedRoute
-                    userRoles={['adventureManager', 'admin']}
-                    path="/oos/:oosNumber"
-                    component={OOSDetail}
-                  />
-                  <AuthorizedRoute
-                    userRoles={['admin']}
-                    path="/oos/:oosNumber/edit"
-                    component={OOSEdit}
-                  />
-                  <AuthorizedRoute
-                    userRoles={['admin']}
-                    path="/oos/import"
-                    component={OOSImport}
-                  />
-                  <NotFound default />
-                </Router>
-              </Container>
-              <Footer />
-            </SideNav>
-          </div>
-        )}
-      </Query>
+                <Container style={{ paddingTop: '6em', paddingBottom: '2em' }}>
+                  <FlashMessages />
+                  <Router>
+                    <Home path="/" />
+                    <Login path="/login" />
+                    <Logout path="/logout" />
+                    <Adventures path="/adventures" />
+                    <AdventureDetail path="/adventures/:id" />
+                    <AuthorizedRoute
+                      userRoles={['adventureManager', 'admin']}
+                      path="/adventures/:id/edit"
+                      component={AdventureEdit}
+                    />
+                    <AuthorizedRoute
+                      userRoles={['adventureManager', 'admin']}
+                      path="/oos"
+                      component={OOSList}
+                    />
+                    <AuthorizedRoute
+                      userRoles={['adventureManager', 'admin']}
+                      path="/oos/:oosNumber"
+                      component={OOSDetail}
+                    />
+                    <AuthorizedRoute
+                      userRoles={['admin']}
+                      path="/oos/:oosNumber/edit"
+                      component={OOSEdit}
+                    />
+                    <AuthorizedRoute
+                      userRoles={['admin']}
+                      path="/oos/import"
+                      component={OOSImport}
+                    />
+                    <NotFound default />
+                  </Router>
+                </Container>
+                <Footer />
+              </SideNav>
+            </div>
+          )}
+        </Query>
+      </ErrorBoundary>
     );
   }
 }
