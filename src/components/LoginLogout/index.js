a7fea2ee429c4ffdeaf48ad3bb5b2c6c1d03a95d
Graham Ballantyne <grahamb@sfu.ca>
2018-12-13 19:40:04 -0800

    Replace UserMenu with just a Logout link for now

--- src/components/LoginLogout/index.js
+++ src/components/LoginLogout/index.js
@@ -3,15 +3,18 @@ import { Link } from '@reach/router';
 import { Query } from 'react-apollo';
 import { Menu } from 'semantic-ui-react';
 import { GET_LOGIN_STATE } from '../../graphql/queries';
-import UserMenu from '../UserMenu';
+// import UserMenu from '../UserMenu';
 
 const LoginLogout = () => {
   return (
     <Query query={GET_LOGIN_STATE}>
       {({ data: { loggedIn } }) => {
         return loggedIn ? (
-          <UserMenu />
+          <Menu.Item>
+            <Link to="/logout">Logout</Link>
+          </Menu.Item>
         ) : (
+          // <UserMenu />
           <Menu.Item>
             <Link to="/login">Login</Link>
           </Menu.Item>
