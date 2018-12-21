d4057f60250ce277b7b66afc9b6b55730552399b
Graham Ballantyne <grahamb@sfu.ca>
2018-12-21 14:19:58 -0800

    Redirect / to /dashboard if logged in

--- src/features/Home/index.js
+++ src/features/Home/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import { Image, Header } from 'semantic-ui-react';
+import { Redirect } from '@reach/router';
+import Query from '../../components/Query';
 import climb from './images/climb.jpg';
+import { GET_LOGIN_STATE } from '../../graphql/queries';
 
 const Home = () => (
-  <div>
-    <Image src={climb} alt="A scout in climbing equipment at PJ 2015" />
-    <Header as="h1" textAlign="center">
-      Get ready for your PJ Adevnture!
-    </Header>
-  </div>
+  <Query query={GET_LOGIN_STATE}>
+    {({ data }) => {
+      if (data.loggedIn) {
+        return <Redirect noThrow to="/dashboard" />;
+      }
+      return (
+        <div>
+          <Image src={climb} alt="A scout in climbing equipment at PJ 2015" />
+          <Header as="h1" textAlign="center">
+            Get ready for your PJ Adevnture!
+          </Header>
+        </div>
+      );
+    }}
+  </Query>
 );
 
 export default Home;
