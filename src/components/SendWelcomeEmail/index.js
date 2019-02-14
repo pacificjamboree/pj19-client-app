399bbe15b9f75aa9027df3446b2fd8d1d54ee722
Graham Ballantyne <grahamb@sfu.ca>
2019-02-13 18:11:54 -0800

    Don't pass `loading` param

--- src/components/SendWelcomeEmail/index.js
+++ src/components/SendWelcomeEmail/index.js
@@ -28,7 +28,7 @@ const SendWelcomeEmail = ({ client, id, children, lastSentAt, refetch }) => (
         onClick: () => {
           sendEmail({ variables: { id } });
         },
-        loading,
+        // loading,
       });
       return (
         <>
