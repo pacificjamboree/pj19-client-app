dc871e58a0fdc4f31dee02dba0a1c35349ea1987
Graham Ballantyne <grahamb@sfu.ca>
2019-03-09 19:51:32 -0800

    Make adventure guide responsive

--- src/features/AdventureGuide/index.js
+++ src/features/AdventureGuide/index.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactMarkdown from 'react-markdown';
 import { Grid, Header, Loader, Table } from 'semantic-ui-react';
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 import slugify from 'slugify';
+
+import styles from './style.module.css';
 
 const QUERY = gql`
   query {
@@ -22,72 +24,96 @@ function flatten(text, child) {
     : React.Children.toArray(child.props.children).reduce(flatten, text);
 }
 
-const AdventureGuide = () => (
-  <Query query={QUERY}>
-    {({ data, error, loading }) => {
-      if (loading) return <Loader active />;
-      if (error) return <p>Error</p>;
+// const AdventureGuide = () => (
+class AdventureGuide extends Component {
+  state = {};
 
-      const SemanticHeader = ({ level, children }) => {
-        const text = children.reduce(flatten, '');
-        return (
-          <Header id={slugify(text).toLowerCase()} as={`h${level}`}>
-            {text}
-          </Header>
-        );
-      };
+  handleContextRef = contextRef => this.setState({ contextRef });
 
-      const SemanticTable = ({ children }) => (
-        <Table celled striped>
-          {children}
-        </Table>
-      );
+  render() {
+    const { contextRef } = this.state;
+    console.log(contextRef);
+    return (
+      <Query query={QUERY}>
+        {({ data, error, loading }) => {
+          if (loading) return <Loader active />;
+          if (error) return <p>Error</p>;
 
-      const TableCell = ({ isHeader, align, children }) => {
-        switch (isHeader) {
-          case true:
-            return <Table.HeaderCell>{children}</Table.HeaderCell>;
+          const SemanticHeader = ({ level, children }) => {
+            if (level === 1) return null;
+            const text = children.reduce(flatten, '');
+            return (
+              <Header id={slugify(text).toLowerCase()} as={`h${level}`}>
+                {text}
+              </Header>
+            );
+          };
 
-          default:
-            return <Table.Cell>{children}</Table.Cell>;
-        }
-      };
+          const SemanticTable = ({ children }) => (
+            <Table celled striped>
+              {children}
+            </Table>
+          );
 
-      const TableRow = ({ children }) => <Table.Row>{children}</Table.Row>;
+          const TableCell = ({ isHeader, align, children }) => {
+            switch (isHeader) {
+              case true:
+                return <Table.HeaderCell>{children}</Table.HeaderCell>;
 
-      const TableHeader = ({ children }) => (
-        <Table.Header>{children}</Table.Header>
-      );
+              default:
+                return <Table.Cell>{children}</Table.Cell>;
+            }
+          };
 
-      const TableBody = ({ children }) => <Table.Body>{children}</Table.Body>;
+          const TableRow = ({ children }) => <Table.Row>{children}</Table.Row>;
 
-      return (
-        <>
-          <Grid>
-            <Grid.Row>
-              <Grid.Column width={4}>
-                <span>Table of Contents</span>
-                <ReactMarkdown source={data.textContent.toc} />
-              </Grid.Column>
-              <Grid.Column width={12}>
-                <ReactMarkdown
-                  source={data.textContent.body}
-                  renderers={{
-                    heading: SemanticHeader,
-                    table: SemanticTable,
-                    tableHead: TableHeader,
-                    tableBody: TableBody,
-                    tableRow: TableRow,
-                    tableCell: TableCell,
-                  }}
-                />
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
-        </>
-      );
-    }}
-  </Query>
-);
+          const TableHeader = ({ children }) => (
+            <Table.Header>{children}</Table.Header>
+          );
+
+          const TableBody = ({ children }) => (
+            <Table.Body>{children}</Table.Body>
+          );
+
+          return (
+            <div id="context" ref={this.handleContextRef}>
+              <Grid stackable>
+                <Grid.Row>
+                  <Grid.Column width={16}>
+                    <Header as="h1">
+                      Pacific Jamboree 2019 Adventure Guide
+                    </Header>
+                  </Grid.Column>
+                </Grid.Row>
+                <Grid.Row>
+                  <Grid.Column width={6}>
+                    <span className={styles.tocHeader}>Table of Contents</span>
+                    <ReactMarkdown
+                      source={data.textContent.toc}
+                      className={styles.toc}
+                    />
+                  </Grid.Column>
+                  <Grid.Column width={8}>
+                    <ReactMarkdown
+                      source={data.textContent.body}
+                      renderers={{
+                        heading: SemanticHeader,
+                        table: SemanticTable,
+                        tableHead: TableHeader,
+                        tableBody: TableBody,
+                        tableRow: TableRow,
+                        tableCell: TableCell,
+                      }}
+                    />
+                  </Grid.Column>
+                </Grid.Row>
+              </Grid>
+            </div>
+          );
+        }}
+      </Query>
+    );
+  }
+}
 
 export default AdventureGuide;
