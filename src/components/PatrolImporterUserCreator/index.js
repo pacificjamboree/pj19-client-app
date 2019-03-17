1ac0254bbe02f1e14973366172d64721f75747f1
Graham Ballantyne <grahamb@sfu.ca>
2019-03-17 16:43:11 -0700

    WIP show patrol scouters without users

--- src/components/PatrolImporterUserCreator/index.js
+++ src/components/PatrolImporterUserCreator/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import gql from 'graphql-tag';
-import { Query, Mutation } from 'react-apollo';
+import { Query } from 'react-apollo';
 
 const query = gql`
   query getImportedPatrolScouters($importId: String!) {
     patrolScouters(filters: { workflowState: active, importId: $importId }) {
       id
-      fullName
+      _id
+      email
+      user {
+        id
+      }
     }
   }
 `;
@@ -16,8 +20,14 @@ const PatrolImporterUserCreator = ({ importId, stepUpdater }) => (
     {({ data, loading, error }) => {
       if (loading) return null;
       if (error) return `Error!: ${error}`;
-
-      return <pre>{JSON.stringify(data, null, 2)}</pre>;
+      const { patrolScouters } = data;
+      const patrolScoutersWithoutUsers = patrolScouters.filter(p => !p.user);
+      const input = patrolScoutersWithoutUsers.map(p => ({
+        username: p.email,
+        oosId: p._id,
+        workflowState: 'defined',
+      }));
+      return <pre>{JSON.stringify(input, null, 2)}</pre>;
     }}
   </Query>
 );
