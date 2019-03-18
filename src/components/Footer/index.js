fd16af2dcd9f66e5f64e00491137458f3d084471
Graham Ballantyne <grahamb@sfu.ca>
2019-03-17 17:27:23 -0700

    Fix links in Footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,6 +7,7 @@ import {
   List,
   Icon,
 } from 'semantic-ui-react';
+import { Link } from '@reach/router';
 
 const socialIconStyle = { color: 'white' };
 
@@ -15,11 +16,21 @@ const Footer = () => (
     <Container>
       <Grid divided inverted stackable>
         <Grid.Row>
-          <Grid.Column width={2}>
+          <Grid.Column width={4}>
             {/* <Header inverted as="h4" content="PJ Adventure" /> */}
             <List link inverted>
-              <List.Item as="a">Adventure Guide</List.Item>
-              <List.Item as="a">FAQ</List.Item>
+              <List.Item>
+                <Link to="/adventures">Adventures</Link>
+              </List.Item>
+              <List.Item>
+                <Link to="/guide">Adventure Guide</Link>
+              </List.Item>
+              <List.Item>
+                <Link to="/faq">FAQ</Link>
+              </List.Item>
+              <List.Item>
+                <Link to="/contact">Contact Us</Link>
+              </List.Item>
             </List>
           </Grid.Column>
           <Grid.Column width={3}>
