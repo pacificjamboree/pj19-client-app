26bd51b241aa962d0e91f0691c6f73a4d812635a
Graham Ballantyne <grahamb@sfu.ca>
2019-04-07 17:26:12 -0700

    Add PatrolAdventureSelectionView

--- src/features/PatrolScouterDashboard/index.js
+++ src/features/PatrolScouterDashboard/index.js
@@ -5,6 +5,7 @@ import Nav from './Nav';
 import NotFound from '../NotFound';
 import Home from './Home';
 import AdventureSelectionEdit from '../AdventureSelectionEdit';
+import AdventureSelectionView from '../AdventureSelectionView';
 
 const PatrolScouterDashbaord = ({ user }) => {
   return (
@@ -16,6 +17,10 @@ const PatrolScouterDashbaord = ({ user }) => {
         <Grid.Column width={13}>
           <Router>
             <Home user={user} path="/" />
+            <AdventureSelectionView
+              user={user}
+              path="/adventureSelection/:id"
+            />
             <AdventureSelectionEdit
               user={user}
               path="/adventureSelection/:id/edit"
