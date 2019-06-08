13e4271fd56f25172a060f591825465aa21ff41f
Graham Ballantyne <grahamb@sfu.ca>
2019-06-07 21:59:31 -0700

    TEMP Add calendar view of patrol schedule

---
+++ src/components/PatrolScheduleCalendarView/index.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import BigCalendar from 'react-big-calendar';
+import TimeGrid from 'react-big-calendar/lib/TimeGrid';
+import addDays from 'date-fns/add_days';
+import eachDay from 'date-fns/each_day';
+import moment from 'moment';
+import { Icon } from 'semantic-ui-react';
+
+const localizer = BigCalendar.momentLocalizer(moment);
+
+class MyWeek extends React.Component {
+  render() {
+    let { date } = this.props;
+    let range = MyWeek.range(date);
+
+    return <TimeGrid {...this.props} range={range} eventOffset={15} />;
+  }
+}
+
+MyWeek.range = date => eachDay(date, addDays(date, 5));
+
+MyWeek.navigate = (date, action) => {
+  switch (action) {
+    case BigCalendar.Navigate.PREVIOUS:
+      return addDays(date, -5);
+
+    case BigCalendar.Navigate.NEXT:
+      return addDays(date, 5, 'day');
+
+    default:
+      return date;
+  }
+};
+
+MyWeek.title = date => {
+  return `Adventure Schedule`;
+};
+
+const eventStyleGetter = event => {
+  const { location, premiumAdventure } = event.adventure;
+  const backgroundColor = location === 'onsite' ? '#00b5ad' : '#5700b5';
+  const borderColor = location === 'onsite' ? '#1f9c96' : '#5c1f9c';
+  const style = {
+    backgroundColor,
+    borderColor,
+    cursor: 'unset',
+  };
+  return { style };
+};
+
+const MyCalendar = ({ schedule }) => {
+  return (
+    <BigCalendar
+      localizer={localizer}
+      events={schedule.periods}
+      startAccessor={event => new Date(event.startAt)}
+      endAccessor={event => new Date(event.endAt)}
+      titleAccessor={event => {
+        return (
+          <>
+            {event.adventure.premiumAdventure && (
+              <Icon name="star" color="yellow" />
+            )}
+            <span>{event.adventure.fullName}</span>
+          </>
+        );
+      }}
+      eventPropGetter={eventStyleGetter}
+      defaultDate={new Date(2019, 6, 7)}
+      min={new Date(2019, 6, 7, 7, 0)}
+      max={new Date(2019, 6, 7, 18, 0)}
+      defaultView={BigCalendar.Views.WEEK}
+      toolbar={false}
+      views={{ month: false, week: MyWeek, day: false, agenda: false }}
+    />
+  );
+};
+
+export default MyCalendar;
--- src/features/PatrolDetail/index.js
+++ src/features/PatrolDetail/index.js
@@ -6,7 +6,9 @@ import { Container, Grid, Header, Loader } from 'semantic-ui-react';
 import DocumentTitle from '../../components/DocumentTitle';
 import NotFound from '../NotFound';
 import PatrolDetailTable from '../../components/PatrolDetailTable';
+import PatrolScheduleCalendarView from '../../components/PatrolScheduleCalendarView';
 
+import 'react-big-calendar/lib/css/react-big-calendar.css';
 import styles from './styles.module.css';
 
 const GET_PATROL = gql`
@@ -36,6 +38,17 @@ const GET_PATROL = gql`
         _id
         workflowState
       }
+      schedule {
+        periods {
+          startAt
+          endAt
+          adventure {
+            fullName
+            location
+            premiumAdventure
+          }
+        }
+      }
     }
   }
 `;
@@ -54,39 +67,47 @@ const PatrolDetail = ({ patrolNumber }) => (
             patrolNumber,
             patrolScouter,
             workflowState,
+            schedule,
           } = data.patrol;
           return (
-            <Grid stackable divided columns={16}>
-              <Grid.Column width={10}>
-                <Header as="h1">Patrol {patrolNumber}</Header>
-                <PatrolDetailTable admin={true} patrol={data.patrol} />
-              </Grid.Column>
-              <Grid.Column width={6}>
-                <div className={styles.metadata}>
-                  <p>
-                    Patrol ID:
-                    <br />
-                    {id}
-                  </p>
-                  <p>
-                    Database ID:
-                    <br />
-                    {_id}
-                  </p>
-                  <p>
-                    Patrol Scouter ID:
-                    <br />
-                    {patrolScouter.id}
-                  </p>
-                  <p>
-                    Patrol Scouter User ID:
-                    <br />
-                    {patrolScouter.user.id}
-                  </p>
-                  <p>Workflow State: {workflowState}</p>
-                </div>
-              </Grid.Column>
-            </Grid>
+            <>
+              <Grid stackable divided columns={16}>
+                <Grid.Column width={10}>
+                  <Header as="h1">Patrol {patrolNumber}</Header>
+                  <PatrolDetailTable admin={true} patrol={data.patrol} />
+                </Grid.Column>
+                <Grid.Column width={6}>
+                  <div className={styles.metadata}>
+                    <p>
+                      Patrol ID:
+                      <br />
+                      {id}
+                    </p>
+                    <p>
+                      Database ID:
+                      <br />
+                      {_id}
+                    </p>
+                    <p>
+                      Patrol Scouter ID:
+                      <br />
+                      {patrolScouter.id}
+                    </p>
+                    <p>
+                      Patrol Scouter User ID:
+                      <br />
+                      {patrolScouter.user.id}
+                    </p>
+                    <p>Workflow State: {workflowState}</p>
+                  </div>
+                </Grid.Column>
+              </Grid>
+
+              <div style={{ height: 'calc(100vh - 100px)' }}>
+                <PatrolScheduleCalendarView schedule={schedule} />
+              </div>
+              {/* <pre>{JSON.stringify(schedule, null, 2)}</pre> */}
+            </>
           );
         }}
       </Query>
